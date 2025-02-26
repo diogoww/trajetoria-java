701cd29bb34adcc61b790350341603b10124d570
diogo varaschin <diogovaraschin@outlook.com>
2025-02-26 10:52:51 -0300

    controle de fluxo concluido

---
+++ untitled/src/controledefluxo/excecoesPt2.java
@@ -0,0 +1,16 @@
+package controledefluxo;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+
+public class excecoesPt2 {
+    public static void main(String[] args) {
+        try {
+            Number valor = NumberFormat.getInstance().parse("1.75");
+            Double vd = valor.doubleValue();
+            System.out.println(vd);
+        } catch (ParseException e) {
+            System.err.println("numero invalido");
+        }
+    }
+}
