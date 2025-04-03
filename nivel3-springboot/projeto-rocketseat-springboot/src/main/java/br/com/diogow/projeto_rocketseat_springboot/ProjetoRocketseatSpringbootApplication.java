0af23504942f3fabe12b9557f52323e7c54ec4f9
diogo varaschin <diogovaraschin@outlook.com>
2025-04-03 16:14:15 -0300

    add @ComponentScan no projeto

--- nivel3-springboot/projeto-rocketseat-springboot/src/main/java/br/com/diogow/projeto_rocketseat_springboot/ProjetoRocketseatSpringbootApplication.java
+++ nivel3-springboot/projeto-rocketseat-springboot/src/main/java/br/com/diogow/projeto_rocketseat_springboot/ProjetoRocketseatSpringbootApplication.java
@@ -2,8 +2,10 @@ package br.com.diogow.projeto_rocketseat_springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan (basePackages = "br.com.diogow")
 public class ProjetoRocketseatSpringbootApplication {
 
 	public static void main(String[] args) {
