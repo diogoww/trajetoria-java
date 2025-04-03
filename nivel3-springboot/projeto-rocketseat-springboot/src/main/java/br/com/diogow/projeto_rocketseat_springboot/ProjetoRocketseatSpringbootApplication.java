359754c8a6ba71dd7d230c9efb1a51975f4fd3cd
diogo varaschin <diogovaraschin@outlook.com>
2025-04-03 11:24:55 -0300

    refazendo a base do projeto spring boot

---
+++ nivel3-springboot/projeto-rocketseat-springboot/src/main/java/br/com/diogow/projeto_rocketseat_springboot/ProjetoRocketseatSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.diogow.projeto_rocketseat_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoRocketseatSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoRocketseatSpringbootApplication.class, args);
+	}
+
+}
---
+++ nivel3-springboot/projeto-rocketseat-springboot/src/test/java/br/com/diogow/projeto_rocketseat_springboot/ProjetoRocketseatSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diogow.projeto_rocketseat_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoRocketseatSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
