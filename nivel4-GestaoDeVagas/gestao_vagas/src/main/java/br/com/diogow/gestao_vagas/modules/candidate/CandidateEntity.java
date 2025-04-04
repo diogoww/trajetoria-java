75d38adf60f84bdb0a1f630997996703066cabe4
diogo varaschin <diogovaraschin@outlook.com>
2025-04-04 17:55:41 -0300

    Nivel 4 Gestão de Vagas

---
+++ nivel4-GestaoDeVagas/gestao_vagas/src/main/java/br/com/diogow/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -0,0 +1,72 @@
+package br.com.diogow.gestao_vagas.modules.candidate;
+
+import java.util.UUID;
+
+
+public class CandidateEntity {
+
+    private UUID id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private String description;
+    private String curriculum;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCurriculum() {
+        return curriculum;
+    }
+
+    public void setCurriculum(String curriculum) {
+        this.curriculum = curriculum;
+    }
+
+}
