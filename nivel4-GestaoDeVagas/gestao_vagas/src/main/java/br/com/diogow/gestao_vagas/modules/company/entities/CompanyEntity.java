04f6689894fe793cd82e7c47724c922bb62458a0
diogo varaschin <diogovaraschin@outlook.com>
2025-04-10 14:58:48 -0300

    UseCase: CreateCompany

--- nivel4-GestaoDeVagas/gestao_vagas/src/main/java/br/com/diogow/gestao_vagas/modules/company/entities/CompanyEntity.java
+++ nivel4-GestaoDeVagas/gestao_vagas/src/main/java/br/com/diogow/gestao_vagas/modules/company/entities/CompanyEntity.java
@@ -15,7 +15,6 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity(name = "company")
-@Data
 public class CompanyEntity {
 
     @Id
@@ -34,6 +33,71 @@ public class CompanyEntity {
     private String password;
 
     private String website;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
     private String description;
 
     @CreationTimestamp
