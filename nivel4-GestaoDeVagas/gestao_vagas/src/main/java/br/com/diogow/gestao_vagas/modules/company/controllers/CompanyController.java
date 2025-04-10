97ea4c626d030d4a2968fd817a0eacde6ba33e5a
diogo varaschin <diogovaraschin@outlook.com>
2025-04-10 15:35:13 -0300

    create the JobController

--- nivel4-GestaoDeVagas/gestao_vagas/src/main/java/br/com/diogow/gestao_vagas/modules/company/controllers/CompanyController.java
+++ nivel4-GestaoDeVagas/gestao_vagas/src/main/java/br/com/diogow/gestao_vagas/modules/company/controllers/CompanyController.java
@@ -2,6 +2,7 @@ package br.com.diogow.gestao_vagas.modules.company.controllers;
 
 import br.com.diogow.gestao_vagas.modules.company.entities.CompanyEntity;
 import br.com.diogow.gestao_vagas.modules.company.useCases.CreateCompanyUseCase;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +20,7 @@ public class CompanyController {
     private CreateCompanyUseCase createCompanyUseCase;
 
     @PostMapping("/")
-    public ResponseEntity<Object> create(@RequestBody CompanyEntity companyEntity){
+    public ResponseEntity<Object> create(@Valid @RequestBody CompanyEntity companyEntity){
        try {
            var result = this.createCompanyUseCase.execute(companyEntity);
            return ResponseEntity.ok().body(result);
