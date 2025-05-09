34a1810c7bf64c6bd6988ed07fcefdf8ba04a701
diogo varaschin <diogovaraschin@outlook.com>
2025-05-09 11:02:17 -0300

    add getters and setter in the JobEntity

--- nivel4-GestaoDeVagas/gestao_vagas/src/main/java/br/com/diogow/gestao_vagas/modules/company/entities/JobEntity.java
+++ nivel4-GestaoDeVagas/gestao_vagas/src/main/java/br/com/diogow/gestao_vagas/modules/company/entities/JobEntity.java
@@ -25,6 +25,62 @@ public class JobEntity {
     @JoinColumn(name = "company_id", insertable = false, updatable = false)
     private CompanyEntity companyEntity;
 
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public UUID getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(UUID companyId) {
+        this.companyId = companyId;
+    }
+
+    public CompanyEntity getCompanyEntity() {
+        return companyEntity;
+    }
+
+    public void setCompanyEntity(CompanyEntity companyEntity) {
+        this.companyEntity = companyEntity;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getBenefits() {
+        return benefits;
+    }
+
+    public void setBenefits(String benefits) {
+        this.benefits = benefits;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     @Column(name = "company_id", nullable = false)
     private UUID companyId;
 
