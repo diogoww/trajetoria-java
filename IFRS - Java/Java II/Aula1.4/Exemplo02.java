ff3b7d93c4272e4a9c4d1039a04250d5f4a4575e
Diogo Varaschin <diogovaraschin@outlook.com>
2025-08-12 10:37:28 -0300

    Java II - Aula 1.4 | Exemplo 02

---
+++ IFRS - Java/Java II/Aula1.4/Exemplo02.java
@@ -0,0 +1,21 @@
+public class Exemplo02 {
+    public static void main(String[] args) {
+
+        char conceito;
+
+        System.out.print("Conceito do aluno: ");
+        conceito = System.console().readLine().charAt(0);
+
+        if(conceito != 'A' && conceito != 'B' && conceito != 'C'){
+            System.out.println("Conceito inválido");
+        }
+
+        if(conceito == 'A' || conceito == 'B'){
+            System.out.println("Aluno aprovado");
+        }
+
+        if(conceito == 'C'){
+            System.out.println("Aluno reprovado");
+        }
+    }
+}
