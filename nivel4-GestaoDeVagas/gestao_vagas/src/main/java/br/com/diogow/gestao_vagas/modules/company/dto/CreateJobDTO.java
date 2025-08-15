d9f599fb248914558f6bce77709652f7a04606ff
Diogo Varaschin <diogovaraschin@outlook.com>
2025-08-15 10:42:43 -0300

    feat: insert examples in requests

--- nivel4-GestaoDeVagas/gestao_vagas/src/main/java/br/com/diogow/gestao_vagas/modules/company/dto/CreateJobDTO.java
+++ nivel4-GestaoDeVagas/gestao_vagas/src/main/java/br/com/diogow/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -1,10 +1,16 @@
 package br.com.diogow.gestao_vagas.modules.company.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class CreateJobDTO {
+    @Schema(example = "Vaga para pessoal desenvolvedora JR", requiredMode = Schema.RequiredMode.REQUIRED)
     private String description;
+
+    @Schema(example = "Plano de saúde, GYMPass, Vale Alimentação", requiredMode = Schema.RequiredMode.REQUIRED)
     private String benefits;
+
+    @Schema(example = "JUNIOR", requiredMode = Schema.RequiredMode.REQUIRED)
     private String level;
 }
--- nivel4-GestaoDeVagas/gestao_vagas/src/main/java/br/com/diogow/gestao_vagas/modules/company/entities/JobEntity.java
+++ nivel4-GestaoDeVagas/gestao_vagas/src/main/java/br/com/diogow/gestao_vagas/modules/company/entities/JobEntity.java
@@ -1,5 +1,6 @@
 package br.com.diogow.gestao_vagas.modules.company.entities;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -21,71 +22,19 @@ public class JobEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Schema(example = "Vaga para dev web")
     private String description;
+    @Schema(example = "Plano de saúde, GYMPass, Vale Alimentação")
     private String benefits;
 
     @NotBlank(message = "Esse campo é obrigatório.")
+    @Schema(example = "PLENO")
     private String level;
 
     @ManyToOne()
     @JoinColumn(name = "company_id", insertable = false, updatable = false)
     private CompanyEntity companyEntity;
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public UUID getCompanyId() {
-        return companyId;
-    }
-
-    public void setCompanyId(UUID companyId) {
-        this.companyId = companyId;
-    }
-
-    public CompanyEntity getCompanyEntity() {
-        return companyEntity;
-    }
-
-    public void setCompanyEntity(CompanyEntity companyEntity) {
-        this.companyEntity = companyEntity;
-    }
-
-    public String getLevel() {
-        return level;
-    }
-
-    public void setLevel(String level) {
-        this.level = level;
-    }
-
-    public String getBenefits() {
-        return benefits;
-    }
-
-    public void setBenefits(String benefits) {
-        this.benefits = benefits;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 
     @Column(name = "company_id", nullable = false)
     private UUID companyId;
